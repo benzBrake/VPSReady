74dc196a49b3cf9fa178c0ddf743a98885bcfdaf
Ryan Lieu <github-benzBrake@woai.ru>
2021-08-10 16:51:25 +0800

    docs: 去除 docker.sh 注释

--- .init/docker.sh
+++ .init/docker.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
-###
- # @Author: Ryan
- # @Date: 2021-02-22 20:48:42
- # @LastEditTime: 2021-02-22 20:49:21
- # @LastEditors: Ryan
- # @Description: 
- # @FilePath: \VPSReady\docker.sh
-### 
 bash -c "$(curl -fsSL https://get.docker.com -o -)"
 apt-get update
 apt-get install docker-compose
