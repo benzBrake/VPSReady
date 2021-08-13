78c82e964a4d48ac839ca80ef0e32f57c352617c
Ryan Lieu <github-benzBrake@woai.ru>
2021-08-13 16:02:14 +0800

    fix: invalid number of arguments in "worker_processes"

--- .init/nginx.sh
+++ .init/nginx.sh
@@ -2,5 +2,5 @@
 if [ ! -f /data/web/nginx-frontend.conf ]; then
   cp /data/web/demo-config/web.conf /data/web/nginx-frontend.conf
   CORES=$(cat /proc/cpuinfo | grep processor | wc -l)
-  sed -i "s/worker_processes.*/worker_processes  ${CORES}/" /data/web/nginx-frontend.conf
+  sed -i "s/worker_processes.*/worker_processes  ${CORES};/" /data/web/nginx-frontend.conf
 fi
